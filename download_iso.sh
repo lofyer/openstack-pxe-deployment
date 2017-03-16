7ba84b5f8d255833af7a7ea7c1f7520099eea064
lofyer <lofyer@gmail.com>
2017-03-16 11:30:08 +0800

    update

---
+++ download_iso.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -nc -c http://mirrors.ustc.edu.cn/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1611.iso -P webroot/centos/iso/
+wget -nc -c http://mirrors.ustc.edu.cn/ubuntu-releases/xenial/ubuntu-16.04.2-desktop-amd64.iso -P webroot/ubuntu/iso/
